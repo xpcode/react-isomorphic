4f8afbddeb538322b260efda98b06a3a00bc925d
chenhf <4019980@qq.com>
2016-07-16 10:21:27 +0800

    update

--- examples/env/index.jsx
+++ examples/env/index.jsx
@@ -1,2 +1,23 @@
-// server、client 判断是否是开发环境
-var isDev = process.env.NODE_ENV !== 'production'
+// 判断是否是开发环境
+process.env.NODE_ENV !== 'production'
+
+// 判断是否是客户端环境
+process.env.__CLIENT__ !== true
+
+// koa 的 controller 写法
+// 调用异步接口 queryUserByFromServices 后，把数据传递给 redux，然后 render
+router.get('/users', async function (ctx) {
+  const pageInfo = {
+    title: '用户列表',
+    keyword: '',
+    description: '',
+  }
+
+  await queryUserByFromServices().then(json => {
+    ctx.store.dispatch({
+      type: actions.U8_USER_GETLIST_SUCCESS,
+      payload: json
+    })
+    ctx.render(pageInfo)
+  })
+})
